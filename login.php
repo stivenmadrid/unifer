ca4cc60d7efbcc54f5dc327d7ebcf77338dc19c9
stiven madrid <esmadrid2@misena.edu.co>
2021-11-12 07:37:09 -0500

    se cambio todo el login

--- login.php
+++ login.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
     <title>login</title>
-    <link rel="stylesheet" type="text/css" href="css/styles_login.css">
-    <link href="bstra/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="css/login2.css">
+    <link href="bstra/css/sb-admin-2.min.css" rel="stylesheet">
+
 </head> 
 <div class="h2">  </div>      
 <body >
@@ -15,10 +16,14 @@
 
    <div class="login">
    
-	<h1>Login</h1>
+	<h1 align="center">Login</h1>
     <form  action="servicios/login.php" method="POST"  name="form1" onsubmit="return vlidar1()">
     	<input type="text" placeholder="correo eletronico" id="correo1" name="emausu" required="required" />
+        <br>
+        <br>
         <input type="password"  placeholder="contraseña"  id="contraseña3" name="pasusu" required="required" />
+        <br>
+        <br>
         <button type="submit" id="Ingresar" class="btn btn-primary btn-block btn-large">Ingresar</button>
     </form>
    
