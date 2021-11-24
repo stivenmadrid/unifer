f7633b03246bc3ee1b447bde3f781f8071a6a9ad
wilmer <waperez236@misena.edu.co>
2021-11-24 17:22:28 -0500

    agregarcarrito midificado

--- agregar_carrito.php
+++ agregar_carrito.php
@@ -6,11 +6,7 @@ $cantidad=$_POST["cantidad"];
 // $carrito=array[];
 $_SESSION['id_productos']=$id_productos;
 $_SESSION['cantidad']=$cantidad;
-<<<<<<< HEAD
-// $_SESSION[$carrito];
-=======
 // $=_SESSION[$carrito];
->>>>>>> main
 include ("pagina_principal.php");
 
 
